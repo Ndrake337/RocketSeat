2b4a1b3b0601cda98667dec6f34ed69377520006
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-04-22 23:19:11 -0300

    Adição de variaveis de ambiente na nossa aplicação

--- NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
+++ NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
@@ -1,24 +1,25 @@
 import fastify from 'fastify'
-import crypto from "node:crypto";
+import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex("transactions")
+  const transaction = await knex('transactions')
     .insert({
       id: crypto.randomUUID(),
-      title: "Transação de teste",
+      title: 'Transação de teste',
       amount: 1000,
     })
-    .returning("*");
+    .returning('*')
 
-  return transaction;
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
