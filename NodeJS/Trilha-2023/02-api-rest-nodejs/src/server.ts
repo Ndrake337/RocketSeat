9e3dfb6311e22d63b1cbd6a05d30c863c36a6d38
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-10-21 20:52:04 -0300

    Integração com DB usando Knex e Adição de variaveis de ambiente validadas com o Zod

--- NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
+++ NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,24 @@
 import fastify from "fastify";
+import { knex } from "./database";
+import crypto from "node:crypto";
+import { env } from "./env";
 
 const app = fastify();
 
 // GET, POST, PUT PATCH, DELETE
 
-app.get("/hello", () => {
-  return "Hello World";
+app.get("/hello", async () => {
+  const transaction = knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server Running");
 });
