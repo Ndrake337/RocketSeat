8048218d2e14c3cf5f91f4576f62ac5d587de019
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-10-21 17:40:50 -0300

    Adicionando estrutura básica da aplicação

---
+++ NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+// GET, POST, PUT PATCH, DELETE
+
+app.get("/hello", () => {
+  return "Hello World";
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server Running");
+});
