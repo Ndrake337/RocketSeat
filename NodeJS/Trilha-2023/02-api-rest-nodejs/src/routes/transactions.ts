85386e8fe1dbbc49e672ba1ededa6f226715dabc
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-10-22 21:41:50 -0300

    Rotas de Transactions Implementadas

---
+++ NodeJS/Trilha-2023/02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,101 @@
+import { randomUUID } from "node:crypto";
+import { knex } from "../database";
+import { z } from "zod";
+import { FastifyInstance } from "fastify";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
+
+// Cookies <-> Formas de se manter o contexto entre requisições
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook("preHandler", async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`);
+  });
+
+  app.get(
+    "/",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
+
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
+
+      return { transactions };
+    }
+  );
+
+  app.get(
+    "/:id",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
+
+      const { id } = getTransactionParamsSchema.parse(request.params);
+
+      const transaction = await knex("transactions")
+        .where({ id, session_id: sessionId })
+        .first();
+
+      return { transaction };
+    }
+  );
+
+  app.get(
+    "/summary",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
+      const summary = await knex("transactions")
+        .where("session_id", sessionId)
+        .sum("amount", {
+          as: "amount",
+        })
+        .first();
+
+      return { summary };
+    }
+  );
+
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+      session_id: sessionId,
+    });
+
+    return reply.status(201).send();
+  });
+}
--- NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
+++ NodeJS/Trilha-2023/02-api-rest-nodejs/src/server.ts
@@ -1,22 +1,15 @@
 import fastify from "fastify";
-import { knex } from "./database";
-import crypto from "node:crypto";
+import cookie from "@fastify/cookie";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
 // GET, POST, PUT PATCH, DELETE
 
-app.get("/hello", async () => {
-  const transaction = knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Transação de teste",
-      amount: 1000,
-    })
-    .returning("*");
-
-  return transaction;
+app.register(cookie);
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app.listen({ port: env.PORT }).then(() => {
