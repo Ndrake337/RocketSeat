49c0479756ba60af49edde4c5089471bf42d46e9
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-11-11 16:50:33 -0300

    .

---
+++ NodeJS/Trilha-2023/desafio-02-daily-diet-api/src/database.ts
@@ -0,0 +1,13 @@
+import { knex as setupKnex } from "knex";
+
+export const knex = setupKnex({
+  client: "sqlite3",
+  connection: {
+    filename: "./db/app.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+});
