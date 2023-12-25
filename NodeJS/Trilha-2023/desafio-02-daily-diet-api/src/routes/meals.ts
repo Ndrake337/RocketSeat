dd2b08c3a0587a5c675f45d12a61112eeb4fbe7c
Matheus Felipe Neves Campos Cardoso <matheus.felipe337@gmail.com>
2023-12-24 22:09:58 -0300

    Finishing Meals Routes Without Test

--- NodeJS/Trilha-2023/desafio-02-daily-diet-api/src/routes/meals.ts
+++ NodeJS/Trilha-2023/desafio-02-daily-diet-api/src/routes/meals.ts
@@ -129,4 +129,95 @@ export async function mealsRoutes(app: FastifyInstance) {
         .delete();
     }
   );
+  
+  app.put(
+    "/:mealId",
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() });
+
+      const { mealId } = paramsSchema.parse(request.params);
+
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      });
+
+      const { name, description, isOnDiet, date } = updateMealBodySchema.parse(
+        request.body
+      );
+
+      const meal = await knex("meals").where({ id: mealId }).first();
+
+      if (!meal) {
+        return reply.status(404).send({ error: "Meal not found" });
+      }
+
+      await knex("meals").where({ id: mealId }).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+      });
+
+      return reply.status(204).send();
+    }
+  );
+
+  app.get(
+    "/metrics",
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies;
+
+      const user = await knex("tb_users")
+        .select("*")
+        .where("session_Id", sessionId)
+        .first();
+      if (!user) {
+        return reply.status(401).send({
+          error: "Unauthorized",
+        });
+      }
+
+      const totalMealsOnDiet = await knex("tb_meals")
+        .where({ userId: user.id, isOnDiet: true })
+        .count("id", { as: "total" })
+        .first();
+
+      const totalMealsOffDiet = await knex("tb_meals")
+        .where({ userId: user.id, isOnDiet: false })
+        .count("id", { as: "total" })
+        .first();
+
+      const totalMeals = await knex("tb_meals")
+        .where({ userId: user.id })
+        .orderBy("mealTime", "desc");
+      let { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.isOnDiet === 1) {
+            acc.currentSequence += 1;
+          } else {
+            acc.currentSequence = 0;
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence;
+          }
+
+          return acc;
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 }
+      );
+
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      });
+    }
+  );
 }
